89454a6c73c7e229b55b4c94900b14c41434da03
Avagrace2 <68303045+Avagrace2@users.noreply.github.com>
2020-08-12 12:45:08 -0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,20 @@
+Let num1 = 16;
+Let str1 = "ME";
+Let phrase = `my favorite superhero is ${str1}`;
+Let phrase2 = `I am $[num1] years old`;
+Let IAmTallerThan5Ft5In = true;
+Let fillThisLater;
+
+console.log(phrase2);
+
+// what did we learn today:
+// Syntax for assigning variables
+
+// 7 Data Types:
+//    Numbers 
+//    Strings 
+//    Booleans 
+//    null 
+//    undefined
+//    symbols- not yet 
+//    objects- not yet
